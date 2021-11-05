044f8b4e5f1ac162cd413af59bd7f1faf304cda0
Ricarss <ricarss28@gmail.com>
2021-11-05 15:46:48 +0000

    adición de conteo de pasos

--- binary_search.py
+++ binary_search.py
@@ -11,54 +11,63 @@ def ar_gen():
 def linear_search(ar,obj):
 
     match = False
+    steps = 0
 
     for i in ar:
+        steps += 1
         if i == obj:
             match = True
             break
     
-    return match
+    return match, steps
 
-def binary_search(ar,obj,ini,end):
+def binary_search(ar,obj,ini,end,steps):
+
+    steps += 1
 
     if ini > end:
-        return False
+        return False, steps
 
     medio = (end-ini) // 2
 
     if ar[medio] == obj:
-        return True
+        return True, steps
     
     if ar[medio] > obj:
-        return binary_search(ar,obj,medio+1,end)
+        return binary_search(ar,obj,medio+1,end,steps)
     
     if ar[medio] < obj:
-        return binary_search(ar,obj,ini,medio-1)
+        return binary_search(ar,obj,ini,medio-1,steps)
 
 if __name__ == '__main__':
     
     ar = ar_gen()
     obj = rd.randint(0,55000)
+    
+    ls_ini = t.time()
+    ls_match, ls_steps = linear_search(ar,obj)
+    ls_time = t.time() - ls_ini
 
-    inicio = t.time()
+    bs_ini = t.time()
+    bs_match, bs_steps = binary_search(ar,obj,0,len(ar)-1,0)
+    bs_time = t.time() - bs_ini
+
     print(f"""
-    {obj} {"está" if linear_search(ar,obj) else "no está"} en la lista generada
+    {obj} {"está" if ls_match else "no está"} en la lista generada
     """)
-    fin = t.time()
     
     print(f'''
-    La búsqueda lineal tardó {fin-inicio} segundos    
+    La búsqueda lineal tardó {ls_time} segundos, con un total de {ls_steps} pasos
     ''')
 
     print("\n")
 
     inicio = t.time()
     print(f'''
-    {obj} {'está' if binary_search(ar,obj,0,len(ar)-1) else 'no está'} en la lista generada
+    {obj} {'está' if bs_match else 'no está'} en la lista generada
     ''')
     fin = t.time()
 
     print(f'''
-    La búsqueda binaria tardó {fin - inicio} segundos
+    La búsqueda binaria tardó {bs_time} segundos, con un total de {bs_steps} pasos
     ''')
-
