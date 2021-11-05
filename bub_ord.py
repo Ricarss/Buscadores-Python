e3d7baa36b4336cc737995e790d646d42ffb3d3f
Ricarss <ricarss28@gmail.com>
2021-11-05 19:01:09 +0000

    método de búsqueda simple sin optimizar

--- bub_ord.py
+++ bub_ord.py
@@ -1,16 +1,30 @@
 import random as rd
 
-if __name__ == '__main__':
-    pass
-
 def ar_gen():
     
-    size = rd.randint(1000,1500) # código que genera el tamaño de la lista
-    ar = sorted([rd.randint(0,size) for i in range(size)]) # código que genera el listado 
+    size = rd.randint(5,10) # código que genera el tamaño de la lista
+    ar = [rd.randint(0,size) for i in range(size)] # código que genera el listado 
 
     return ar
 
-
 def bub_sort(ar):
-    pass
+    
+    bubble = 0
+
+    for idx1 in range(len(ar)):
+
+        for idx2 in range(len(ar)-1) :
+
+            if ar[idx2] > ar[idx2+1]:
+
+                bubble = ar[idx2+1]
+                ar[idx2+1] = ar[idx2]
+                ar[idx2] = bubble                
+
+    return ar
+
+if __name__ == '__main__':
+    ar = ar_gen()
+    print(f'El arreglo a ordenar es: \n {ar} \n')
+    print(f'El arreglo ordenado es: \n {bub_sort(ar)}')
 
